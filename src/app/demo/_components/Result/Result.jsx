d88fc874004c3d67e9af524fc72a07b381a6458e
mr10us <minenkoduck@gmail.com>
2025-05-05 16:29:40 +0300

    fix: origin

--- src/app/demo/_components/Result/Result.jsx
+++ src/app/demo/_components/Result/Result.jsx
@@ -1,13 +1,13 @@
 import { Typewriter } from "@/components/animations/Typewriter/Typewriter";
 
 export const Result = ({ title, content, addedAt, origin }) => {
-  const formatedTime = new Date(addedAt).toGMTString()
+  const formatedTime = new Date(addedAt).toGMTString();
   return (
     <div>
       <Typewriter asChild>
-      <h2 className="mb-10 text-4xl lg:text-6xl font-semibold text-primary text-center">
-        Result
-      </h2>
+        <h2 className="mb-10 text-4xl lg:text-6xl font-semibold text-primary text-center">
+          Result
+        </h2>
       </Typewriter>
 
       <Typewriter speed="default" asChild>
@@ -15,8 +15,12 @@ export const Result = ({ title, content, addedAt, origin }) => {
           <p>{formatedTime}</p>
           <h3 className="text-3xl">{title}</h3>
           <div dangerouslySetInnerHTML={{ __html: content }} />
-          <p>Origin: 
-          <a href={origin} className="underline" target="_blank">{origin}</a></p>
+          <p>
+            Origin:{" "}
+            <a href={origin} className="underline" target="_blank">
+              {origin}
+            </a>
+          </p>
         </div>
       </Typewriter>
     </div>
