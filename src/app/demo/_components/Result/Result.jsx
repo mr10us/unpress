375bfc3f6e20e4bf6ffd7799bf1480e5b66bc047
mr10us <minenkoduck@gmail.com>
2025-05-05 14:43:44 +0300

    fix: raw html and stylings

--- src/app/demo/_components/Result/Result.jsx
+++ src/app/demo/_components/Result/Result.jsx
@@ -1,12 +1,20 @@
-export const Result = ({title, content}) => {
+import { Typewriter } from "@/components/animations/Typewriter/Typewriter";
+
+export const Result = ({ title, content }) => {
   return (
     <div>
-      <h2 className="mb-10 text-4xl lg:text-6xl font-semibold text-primary">Result</h2>
+      <Typewriter asChild>
+      <h2 className="mb-10 text-4xl lg:text-6xl font-semibold text-primary text-center">
+        Result
+      </h2>
+      </Typewriter>
 
-      <div className="content card px-10 py-8 text-gray-100">
-        <h3>{title}</h3>
-        {content}
-      </div>
+      <Typewriter asChild>
+        <div className="content card px-10 py-8 text-gray-100">
+          <h3>{title}</h3>
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+        </div>
+      </Typewriter>
     </div>
-  )
-}
+  );
+};
